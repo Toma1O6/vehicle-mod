7a6640b1d129fe8ba490cf8374ea3168b6888aa3
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2019-05-22 11:28:30 +0200

    core

---
+++ src/main/java/dev/toma/vehiclemod/Registries.java
@@ -0,0 +1,90 @@
+package dev.toma.vehiclemod;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.EntityEntry;
+import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.registries.IForgeRegistry;
+
+public class Registries {
+	
+	@ObjectHolder(VehicleMod.Constants.ID)
+	public static final class VMItems {
+		
+	}
+	
+	@ObjectHolder(VehicleMod.Constants.ID)
+	public static final class VMBlocks {
+		
+	}
+	
+	@EventBusSubscriber
+	public static class Registry {
+		
+		static int id = -1;
+		
+		@SubscribeEvent
+		public static void onBlockRegister(RegistryEvent.Register<Block> e) {
+			final Block[] blocks = {
+			};
+		}
+		
+		@SubscribeEvent
+		public static void onItemRegister(RegistryEvent.Register<Item> e) {
+			final Item[] items = {
+			};
+		}
+		
+		@SubscribeEvent
+		public static void onEntityRegister(RegistryEvent.Register<EntityEntry> e) {
+			
+		}
+		
+		private static EntityEntry registerVehicle(String name, Class<? extends Entity> cls) {
+			return createBuilder(name).entity(cls).tracker(256, 1, true).build();
+		}
+		
+		private static <E extends Entity> EntityEntryBuilder<E> createBuilder(String name) {
+			EntityEntryBuilder<E> builder = EntityEntryBuilder.create();
+			ResourceLocation rl = new ResourceLocation(VehicleMod.Constants.ID, name);
+			return builder.id(rl, id++).name(rl.toString());
+		}
+	}
+	
+	@EventBusSubscriber(Side.CLIENT)
+	public static class ModelRegistry {
+		
+		@SubscribeEvent
+		public static void onModelRegister(ModelRegistryEvent e) {
+			final IForgeRegistry<Item> ITEMS = ForgeRegistries.ITEMS;
+			final IForgeRegistry<Block> BLOCKS = ForgeRegistries.BLOCKS;
+			
+			for(ResourceLocation rl : ITEMS.getKeys()) {
+				if(rl.getResourceDomain().equalsIgnoreCase(VehicleMod.Constants.ID)) {
+					registerModel(ITEMS.getValue(rl));
+				}
+			}
+			
+			for(ResourceLocation rl : BLOCKS.getKeys()) {
+				if(rl.getResourceDomain().equalsIgnoreCase(VehicleMod.Constants.ID)) {
+					registerModel(Item.getItemFromBlock(BLOCKS.getValue(rl)));
+				}
+			}
+		}
+		
+		private static void registerModel(Item item) {
+			ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+		}
+	}
+}
---
+++ src/main/java/dev/toma/vehiclemod/VehicleMod.java
@@ -0,0 +1,49 @@
+package dev.toma.vehiclemod;
+
+import org.apache.logging.log4j.Logger;
+
+import dev.toma.vehiclemod.proxy.IProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = VehicleMod.Constants.ID, name = VehicleMod.Constants.NAME, version = VehicleMod.Constants.VERSION)
+public class VehicleMod {
+	
+	@Instance
+	public static VehicleMod instance;
+	
+	@SidedProxy(clientSide = "dev.toma.vehiclemod.proxy.ClientProxy")
+	public static IProxy proxy;
+	
+	public static Logger logger;
+	
+	@EventHandler
+	public static void preInit(FMLPreInitializationEvent e) {
+		logger = e.getModLog();
+		proxy.preInit(e);
+	}
+	
+	@EventHandler
+	public static void init(FMLInitializationEvent e) {
+		proxy.init(e);
+	}
+	
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent e) {
+		proxy.postInit(e);
+	}
+	
+	public static final class Constants {
+		
+		public static final String ID = "vehiclemod";
+		public static final String NAME = "Toma's Vehicle Mod";
+		public static final String VERSION = "1.0";
+		
+		
+	}
+}
