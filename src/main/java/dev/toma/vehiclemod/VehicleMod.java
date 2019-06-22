1507846a87d57837a71b5284461a2c718212af40
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2019-06-22 12:03:16 +0200

    versions

--- src/main/java/dev/toma/vehiclemod/VehicleMod.java
+++ src/main/java/dev/toma/vehiclemod/VehicleMod.java
@@ -82,7 +82,7 @@ public class VehicleMod {
 		
 		public static final String ID = "vehiclemod";
 		public static final String NAME = "Toma's Vehicle Mod";
-		public static final String VERSION = "1.1";
+		public static final String VERSION = "1.1.1";
 		
 		public static final Random RNG = new Random();
 	}
