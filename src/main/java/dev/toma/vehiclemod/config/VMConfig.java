3b934c39c5ac3599b252acc97f17d70545f801c3
Toma <novotny.tom96@gmail.com>
2020-11-03 23:18:17 +0100

    default vehicle stat rebalance

--- src/main/java/dev/toma/vehiclemod/config/VMConfig.java
+++ src/main/java/dev/toma/vehiclemod/config/VMConfig.java
@@ -25,13 +25,13 @@ public class VMConfig {
 	public static SpeedDisplayUnit speedUnit = SpeedDisplayUnit.KILOMETRES_PER_HOUR;
 	
 	@Name("Beamer S120")
-	public static VehicleStats beamer_s120 = new VehicleStats(250F, 1.23F, 0.0049F, 0.018F, 0.15F, 3.0F, 0.004f, 50).track();
+	public static VehicleStats beamer_s120 = new VehicleStats(250F, 1.23F, 0.0049F, 0.017F, 0.15F, 3.0F, 0.004f, 50).track();
 
 	@Name("Fedoratti Vulcan")
-	public static VehicleStats fedoratti_vulcan = new VehicleStats(220F, 1.74F, 0.007F, 0.023F, 0.25F, 3.5F, 0.0076F, 70).track();
+	public static VehicleStats fedoratti_vulcan = new VehicleStats(220F, 1.72F, 0.007F, 0.023F, 0.25F, 3.5F, 0.0076F, 70).track();
 
 	@Name("Tracer T1")
-	public static VehicleStats tracer_t1 = new VehicleStats(250F, 1.52F, 0.0067F, 0.016F, 0.17F, 3.3F, 0.006F, 50).track();
+	public static VehicleStats tracer_t1 = new VehicleStats(250F, 1.41F, 0.0067F, 0.016F, 0.17F, 3.3F, 0.006F, 50).track();
 
 	@Name("Sputnik 2000L")
 	public static VehicleStats sputnik2000L = new VehicleStats(200F, 1.12F, 0.0035F, 0.019F, 0.13F, 3.0F, 0.0062F, 30).track();
@@ -46,10 +46,10 @@ public class VMConfig {
 	public static VehicleStats beamerPickup = new VehicleStats(280F, 0.8F, 0.0025F, 0.012F, 0.1F, 2.5F, 0.0075F, 60, new VehicleStats.Vector3i(0, 0, -2)).track();
 
 	@Name("Sputnik 3000L")
-	public static VehicleStats sputnik3000L = new VehicleStats(250F, 1.4F, 0.0052F, 0.019F, 0.2F, 3.3F, 0.0041F, 50).track();
+	public static VehicleStats sputnik3000L = new VehicleStats(250F, 1.26F, 0.0042F, 0.019F, 0.2F, 3.3F, 0.0041F, 50).track();
 
 	@Name("Sputnik 3000L Enforcer")
-	public static VehicleStats sputnik3000LEnf = new VehicleStats(300F, 1.58F, 0.0057F, 0.020F, 0.21F, 3.3F, 0.0046F, 50).track();
+	public static VehicleStats sputnik3000LEnf = new VehicleStats(300F, 1.28F, 0.0046F, 0.020F, 0.21F, 3.3F, 0.0046F, 50).track();
 
 	@Name("Sputnik Duster")
 	public static VehicleStats sputnikDuster = new VehicleStats(300F, 1.28F, 0.0041F, 0.0165F, 0.18F, 3.0F, 0.008f, 70).track();
@@ -58,10 +58,10 @@ public class VMConfig {
 	public static VehicleStats fedorattiNightStalker = new VehicleStats(200F, 1.58F, 0.0074F, 0.026F, 0.28F, 3.5F, 0.0076F, 60).track();
 
 	@Name("Proton P1")
-	public static VehicleStats protonP1 = new VehicleStats(250F, 1.37F, 0.005F, 0.019F, 0.22F, 3.3F, 0.0076F, 50).track();
+	public static VehicleStats protonP1 = new VehicleStats(250F, 1.37F, 0.0051F, 0.019F, 0.22F, 3.3F, 0.0076F, 50).track();
 
 	@Name("Proton P1 Tunned")
-	public static VehicleStats protonP1_tunned = new VehicleStats(220F, 1.4F, 0.0052F, 0.022F, 0.23F, 3.3F, 0.0076F, 50).track();
+	public static VehicleStats protonP1_tunned = new VehicleStats(220F, 1.4F, 0.0057F, 0.022F, 0.23F, 3.3F, 0.0076F, 50).track();
 
 	@Mod.EventBusSubscriber(modid = VehicleMod.MODID)
 	public static class Event {
