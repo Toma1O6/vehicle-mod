2e046bc7305a7222cf34bc7be3ffc840b0be9372
Toma <novotny.tom96@gmail.com>
2021-04-24 01:25:16 +0200

    update

--- src/main/java/dev/toma/vehiclemod/common/entity/vehicle/internals/MoveController.java
+++ src/main/java/dev/toma/vehiclemod/common/entity/vehicle/internals/MoveController.java
@@ -2,8 +2,7 @@ package dev.toma.vehiclemod.common.entity.vehicle.internals;
 
 import dev.toma.vehiclemod.common.entity.vehicle.EntityDriveable;
 import dev.toma.vehiclemod.common.entity.vehicle.EntityVehicle;
-import dev.toma.vehiclemod.util.DevUtil;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 
 public class MoveController {
 
@@ -11,6 +10,7 @@ public class MoveController {
     private float rotation;
     private float momentum = 0.9F;
     private boolean reversing;
+    private boolean drifting;
 
     public void inputChanged(byte key, boolean value) {
         if (key == EntityDriveable.BACKWARD && value) {
@@ -18,11 +18,14 @@ public class MoveController {
                 reversing = true;
             }
         }
+        if(key == EntityDriveable.HANDBRAKE) {
+            drifting = value;
+        }
     }
 
     public void moveForward(VehicleUpgrades upgrades, double motionX, double motionZ) {
         float maxSpeed = 44.4F / 20f; // km/h
-        float acceleration = 0.0004F; // +speed/tick
+        float acceleration = 0.004F; // +speed/tick
 
         float currentSpeed = (float) Math.sqrt(motionX * motionX + motionZ * motionZ);
         float maxSpeedDelta = (maxSpeed - currentSpeed) / maxSpeed;
@@ -46,31 +49,50 @@ public class MoveController {
     }
 
     public void useHandbrake() {
-        rotation = MathHelper.clamp(rotation * 2.5F, -5.0F, 5.0F);
-        motion *= 0.96F;
+        //rotation = MathHelper.clamp(rotation * 2.5F, -5.0F, 5.0F);
+        //motion *= 0.96F;
     }
 
     public void applyOn(EntityVehicle vehicle) {
         if (vehicle.getPassengers().isEmpty())
             vehicle.clearInput();
         if (!vehicle.hasMovementInput()) {
-            motion *= 0.98F;
-            vehicle.motionX *= 0.98F;
-            vehicle.motionZ *= 0.98F;
+            handleSpeedDecreasing(vehicle);
         }
+        // TODO dynamic rotation limits
         if (vehicle.getEntitySpeed() < 0.01F)
             rotation = 0;
-
-        float moveMomentum = 0.9F;
-        float driftMomentum = 0.4F;
+        float driftMomentum = 0.55F; // TODO dynamic value based on surface
         vehicle.rotationYaw += rotation;
         vehicle.motionY -= 0.1F;
         rotation *= driftMomentum;
-        vehicle.motionX *= 0.95F;
-        vehicle.motionZ *= 0.95F;
-        vehicle.motionX += Math.sin(-vehicle.rotationYaw * VehicleStyle.PI_D_180) * motion;
-        vehicle.motionZ += Math.cos(vehicle.rotationYaw * VehicleStyle.PI_D_180) * motion;
+        Vec3d lookVector = vehicle.getLookVec();
+        if(drifting) {
+            vehicle.motionX += lookVector.x * motion * 0.04;
+            vehicle.motionZ += lookVector.z * motion * 0.04;
+            motion = (float) vehicle.getEntitySpeed();
+        } else {
+            vehicle.motionX = lookVector.x * motion;
+            vehicle.motionZ = lookVector.z * motion;
+        }
+    }
 
-        //motion = 0;
+    private void handleSpeedDecreasing(EntityVehicle vehicle) {
+        double motionDecrease = 0.001;
+        if(vehicle.motionX != 0) {
+            if(Math.abs(vehicle.motionX) <= motionDecrease) {
+                vehicle.motionX = 0;
+            } else {
+                vehicle.motionX += vehicle.motionX > 0 ? -motionDecrease : motionDecrease;
+            }
+        }
+        if(vehicle.motionZ != 0) {
+            if(Math.abs(vehicle.motionZ) <= motionDecrease) {
+                vehicle.motionZ = 0;
+            } else {
+                vehicle.motionZ += vehicle.motionZ > 0 ? -motionDecrease : motionDecrease;
+            }
+        }
+        motion = (float) vehicle.getEntitySpeed();
     }
 }
